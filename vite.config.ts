5455343ec382835abb3381127bb32fe40b9a8258
Wilianjcr <116184470+Wilianjcr@users.noreply.github.com>
2026-05-31 00:30:15 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ export default defineConfig({
   plugins: [react()],
   server: { port: 3000 },
   build: { outDir: 'dist', sourcemap: false },
-  base: process.env.GH_PAGES ? '/papel-de-pao/' : '/',
+  base: process.env.VITE_BASE || '/',
 });
